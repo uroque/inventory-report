df70aaf7bfe512370c7f066c896a8f51afa598ff
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-09 12:34:47 -0300

    test: implements test_relatorio_produto function

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Pasta de amendoim",
+        "Amenduim",
+        "2022/10/30",
+        "2023/10/30",
+        "00000001",
+        "Ao abrigo de luz solar"
+    )
+
+    assert(
+        product.__repr__() ==
+        f"O produto {product.nome_do_produto}"
+        + f" fabricado em {product.data_de_fabricacao}"
+        + f" por {product.nome_da_empresa} com validade"
+        + f" até {product.data_de_validade}"
+        + f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
