e31ecb44874c3bcb4aaffcd5689916bc2d83ada6
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-22 15:15:23 -0300

    feat: implements methods for read and use json files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,9 @@
 import csv
+import json
+
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
 
 class Inventory:
     def csv_read(filepath):
@@ -8,9 +11,18 @@ class Inventory:
             reader = list(csv.DictReader(csv_file))
             return reader
 
+    def json_read(filepath):
+        with open(filepath, 'r') as file:
+            reader = json.load(file)
+            return reader
+
     @classmethod
     def data_reader(cls, filepath):
+        if filepath.endswith(".csv"):
         return cls.csv_reader(filepath)
+        elif filepath.endswith(".json"):
+            return cls.json_reader(filepath)
+
     @classmethod
     def import_data(cls, filepath, type):
         data = cls.data_reader(filepath)
