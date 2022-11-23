6754ce7e8421b9c0fc0bca9d009b0616e9b8a3f7
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-23 13:14:13 -0300

    feat: implements method to read xml files on Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -16,12 +17,19 @@ class Inventory:
             reader = json.load(file)
             return reader
 
+    def xml_read(filepath):
+        with open(filepath, 'r') as file:
+            reader = xmltodict.parse(file.read())
+            return reader["dataset"]["record"]
+
     @classmethod
     def data_reader(cls, filepath):
         if filepath.endswith(".csv"):
             return cls.csv_read(filepath)
         elif filepath.endswith(".json"):
             return cls.json_read(filepath)
+        elif filepath.endswith(".xml"):
+            return cls.xml_read(filepath)
 
     @classmethod
     def import_data(cls, filepath, type):
