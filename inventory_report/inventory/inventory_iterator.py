c2ad31d1ac9273011e7d06689c40664f5b10016c
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-29 11:12:40 -0300

    feat: implements inventory_iterator class

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,19 @@
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+
+    def __init__(self, products):
+        self.products = products
+        self.index = 0
+
+    def __next__(self):
+        try:
+            product = self.products[self.index]
+
+        except IndexError:
+            raise StopIteration()
+
+        else:
+            self.index += 1
+            return product
