ff711f114c6fc779e7e19fcd467ae7b329efa166
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-29 12:34:29 -0300

    feat: implements main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+import sys
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    _, filepath, report_type = sys.argv
+
+    if filepath.endswith(".xml"):
+        report = InventoryRefactor(XmlImporter)
+
+    if filepath.endswith(".csv"):
+        report = InventoryRefactor(CsvImporter)
+
+    if filepath.endswith(".json"):
+        report = InventoryRefactor(JsonImporter)
+
+    return sys.stdout.write(report.import_data(filepath, report_type))
